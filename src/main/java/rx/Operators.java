689cb807220f6e4858ee4da64720dc1264e7d1b9
Baris Ucakturk <baris.ucakturk@gmail.com>
2019-01-05 16:04:35 +0300

    Add transform, collection, reducing and error operator examples

--- src/main/java/rx/Operators.java
+++ src/main/java/rx/Operators.java
@@ -3,6 +3,7 @@ package rx;
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,15 +19,73 @@ public class Operators {
 
         sleep(2000);
         //skip
-        Observable.range(1,120).skip(100).subscribe(s -> System.out.println("Baris: " + s));
+        Observable.range(1, 120).skip(100).subscribe(s -> System.out.println("Baris: " + s));
 
         //takeWhile
 
-        Disposable strings = Observable.range(1,100)
+        Disposable strings = Observable.range(1, 100)
                 .takeWhile(i -> i < 5)
                 .subscribe(i -> System.out.println("RECEIVED: " + i));
 
         strings.dispose();
+
+        //distinct
+
+        Observable.just("Ozgur", "Baris", "Deniz", "Ayse", "Fatma").map(String::length).distinct().count().subscribe(c -> System.out.println(c));
+        Observable.just("Ozgur", "Baris", "Deniz", "Ayse", "Fatma").map(String::length).distinct().subscribe(s -> System.out.println("Distinct count : " + s));
+
+        Observable.just("Ozgur", "Baris", "Deniz", "Ayse", "Fatma", "Joe")
+                .map(String::length)
+                .distinctUntilChanged()
+                .subscribe(i -> System.out.println("RECEIVED: " + i));
+
+        //elementAt
+        Observable.just("Alpha", "Beta", "Zeta", "Eta", "Gamma",
+                "Delta")
+                .elementAt(4)
+                .subscribe(i -> System.out.println("RECEIVED: " + i));
+
+        //cast
+        Observable<Object> items =
+                Observable.just("Alpha", "Beta", "Gamma").cast(Object.class);
+
+
+        Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon").filter(s -> s.startsWith("Z"))
+                .defaultIfEmpty("None")
+                .subscribe(System.out::println);
+        //sorted
+        Observable.just(1, 2, 4, 6, 7, 87, 88, 6).sorted(Comparator.reverseOrder()).subscribe(s -> System.out.println("Received: " + s));
+
+        //scan
+        Observable.just(5, 3, 7, 10, 2, 14)
+                .scan((accumulator, next) -> accumulator * next)
+                .subscribe(s -> System.out.println("Received: " + s));
+
+        //reduce
+        Observable.just(5, 5, 4, 2, 7, 8, 8, 9).reduce("", (total, next) -> total + (total.equals("") ? "" : ":") + next).subscribe(s -> System.out.println(s));
+
+        //all
+        Observable.just(5, 3, 7, 11, 2, 14)
+                .all(i -> i < 10)
+                .subscribe(s -> System.out.println("Received: " + s));
+
+        //toMap
+        Observable.just("Alpha", "Beta", "Gamma", "Delta",
+                "Epsilon")
+                .toMap(s -> s.charAt(0), String::length)
+                .subscribe(s -> System.out.println("Received: " + s));
+
+        //toMultiMap
+        Observable.just("xy", "yz","tz","tyz","z").toMultimap(String::length).subscribe(s -> System.out.println(s));
+
+        //onError
+        Observable.just(5, 2, 4, 0, 3, 2, 8)
+                .map(i -> 10 / i)
+                .onErrorReturn(e -> - 1)
+                .subscribe(i -> System.out.println("RECEIVED: " + i),
+                        e -> System.out.println("RECEIVED ERROR: " + e)
+                );
+
 
     }
 
